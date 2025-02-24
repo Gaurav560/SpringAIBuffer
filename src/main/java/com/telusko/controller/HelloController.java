343baea9aa8e0b8ad3b2a8055e19537a35ff1556
gaurav <gsharma1154@gmail.com>
2025-02-24 13:23:33 +0530

    role based prompts

--- src/main/java/com/telusko/controller/HelloController.java
+++ src/main/java/com/telusko/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.telusko.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -77,5 +80,35 @@ public class HelloController {
                 .getResult()
                 .getOutput()
                 .getText();
+    }
+
+
+    //role based prompts
+    @GetMapping("/sports")
+    public String getSportsDetail(@RequestParam String name) {
+
+        String message = """
+                List the details of the Famous sports personality: %s \s
+                along with their career achievements.
+                And Show the details in readable format\s
+                \s""";
+
+        String systemMessage = """
+                You are a smart virtual assistant.
+                 If some one ask anything beyond sports, just say that you do not know the answer.
+                """;
+
+        UserMessage userMessage = new UserMessage(String.format(message, name));
+        SystemMessage systemMessage1 = new SystemMessage(systemMessage);
+
+        Prompt prompt = new Prompt(List.of(userMessage, systemMessage1));
+
+        return chatClient.prompt(prompt)
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput()
+                .getText();
+
     }
 }
